974c0cf653eb6fa6b7dd1ca8dd71b9c455271b28
G0tem <bindirin@yahoo.com>
2024-09-10 18:25:52 +0500

    add logic connect bd postgres to golang

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	g0tem "github.com/G0tem/G0tem/G0tem"
 	bot "github.com/G0tem/G0tem/bot"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 // album represents data about a record album.
@@ -17,6 +22,10 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
+type nameDB struct {
+	name string
+}
+
 // albums slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
@@ -25,6 +34,10 @@ var albums = []album{
 }
 
 func main() {
+	// Функция логики работы с бд
+	logic_DB()
+
+	// логика бота
 	go bot.RunBot()
 	fmt.Println(g0tem.MyHouse())
 	router := gin.Default()
@@ -33,6 +46,78 @@ func main() {
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
+}
+
+func logic_DB() {
+	// загрузка переменных
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка при загрузке переменных окружения из файла .env")
+	}
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	// подключение к бд
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// запрос на добавление
+	result, err := db.Exec("insert into first_table (name) values ('G0tem')")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result.LastInsertId()) // не поддерживается
+	fmt.Println(result.RowsAffected()) // количество добавленных строк
+
+	// запрос на все строки
+	rows, err := db.Query("select * from first_table")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	namesDB := []nameDB{}
+
+	for rows.Next() {
+		p := nameDB{}
+		err := rows.Scan(&p.name)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		namesDB = append(namesDB, p)
+	}
+	for _, p := range namesDB {
+		fmt.Println(p.name)
+	}
+
+	// запрос на 1 строку
+	row := db.QueryRow("select * from first_table where name = $1", "Юра")
+	nam := nameDB{}
+	err = row.Scan(&nam.name)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(nam.name)
+
+	// обновляем строку с name=G0tem
+	nameUpdate, err := db.Exec("update first_table set name = $1 where name = $2", "Вася", "G0tem")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(nameUpdate.RowsAffected()) // количество обновленных строк
+
+	// удаляем строку с name="Вася"
+	nameDel, err := db.Exec("delete from first_table where name = $1", "Вася")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(nameDel.RowsAffected()) // количество удаленных строк
 }
 
 // getAlbums responds with the list of all albums as JSON.
